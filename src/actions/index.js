eb7064407b12d0bf00b5503591083842184bf564
jacob coccari <jacob.coccari@gmail.com>
2019-05-30 11:42:37 -0700

    added first action and simulated DB

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
 // TODO: add and export your own actions
+
+import Cities from '../cities.js'
+
+export function setFlats() {
+  return {
+    type: 'SET_CITIES'
+    payload: cities;
+  }
+}
---
+++ src/reducers/cities_reducer.js
@@ -0,0 +1,7 @@
+export default function(state, action) {
+  //compute and return the new state.
+  if (state === undefined) {
+    return []
+  }
+  //handle actions
+}
